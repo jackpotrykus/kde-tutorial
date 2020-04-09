fecd49ff2e7aa50721d4cccb608fcdec8b802d3c
jackpotrykus <jackpotrykus@gmail.com>
2020-04-09 10:55:07 -0500

    reorg

--- kde.r
+++ kde.r
@@ -1,8 +1,7 @@
 library(latex2exp)
-# library(magick)
 set.seed(353535)
 
-# === BART SIMPSON DENSITY ===
+# bart sampsion density
 # http://www.math.yorku.ca/~hkj/Teaching/4230/Coverage/density.R
 dbart <- function(x) {
     d1 <- dnorm(x, 0, 1)
@@ -15,7 +14,7 @@ dbart <- function(x) {
     return(f)
 }
 
-
+# randomly draw from the bart simpson density
 rbart <- function(n) {
     ind <- sample(1:6, n, replace = TRUE, prob = c(5, rep(1,5)) / 10)
     mean <- (ind - 2) / 2 - 1
@@ -25,20 +24,16 @@ rbart <- function(n) {
     return(x)
 }
 
-
-# === DENSITY ESIMATION ===
-# Gaussian kernel
+# gaussian kernel evaluated at point x
 k <- function(x) return(dnorm(x))
 
-
-# Estimate the density at point x
+# density estimate at point x, given sampled xs, and bandwidth h
 fhat <- function(x, xs, h) {
     n <- length(xs)
     return(sum(k(((x - xs) / h))) / (n * h))
 }
 
-
-# Calculate the leave-one-out cross-validation score for various bandwidths
+# calculate the leave-one-out cross-validation score for various bandwidths
 loocv <- function(xs, hs) {
     n <- length(xs)
     scores <- vector()
@@ -56,8 +51,7 @@ loocv <- function(xs, hs) {
     return(scores)
 }
 
-
-# Determine the bandwidth which corresponds to minimum loocv score
+# determine the bandwidth which corresponds to minimum loocv score
 optimal_h <- function(loocv_scores, hs) {
     min_cv <- min(loocv_scores)
     indexes <- which(loocv_scores %in% min_cv)
@@ -66,7 +60,7 @@ optimal_h <- function(loocv_scores, hs) {
 }
 
 
-# Produce estimates of the density at all values of x
+# produce estimates of the density at all values of x
 kde <- function(xs, h) {
     vals <- vector()
     for (x in xs) {
@@ -75,58 +69,32 @@ kde <- function(xs, h) {
     return(vals)
 }
 
+# samples
+gamma_data <- sort(rgamma(250, 3, 1))
+bart_data <- sort(rbart(250))
 
-# === PLOT OPTIMAL DENSITY ESTIMATES ===
-
-gamma_data <- rgamma(250, 3, 1)
-gamma_data <- sort(gamma_data)
+# x axes for plot
 gamma_x_axis <- seq(from = min(gamma_data), to = max(gamma_data), length = 250)
-gamma_test_bandwidths <- seq(from = 0.10, to = 0.90, length = 250)
-gamma_loocv_scores <- loocv(gamma_data, gamma_test_bandwidths)
-gamma_optimal_bandwidth <- optimal_h(gamma_loocv_scores, gamma_test_bandwidths)
-gamma_density_estimate <- kde(gamma_data, gamma_optimal_bandwidth)
-
-bart_data <- rbart(250)
-bart_data <- sort(bart_data)
 bart_x_axis <- seq(from = -3, to = 3, length = 250)
+
+# bandwidths to test
+gamma_test_bandwidths <- seq(from = 0.10, to = 0.90, length = 250)
 bart_test_bandwidths <- seq(from = 0.01, to = 0.20, length = 250)
+
+# loocv scores for bandwidths
+gamma_loocv_scores <- loocv(gamma_data, gamma_test_bandwidths)
 bart_loocv_scores <- loocv(bart_data, bart_test_bandwidths)
+
+# optimal bandwidths
+gamma_optimal_bandwidth <- optimal_h(gamma_loocv_scores, gamma_test_bandwidths)
 bart_optimal_bandwidth <- optimal_h(bart_loocv_scores, bart_test_bandwidths)
+
+# density estimates with optimal bandwidths
+gamma_density_estimate <- kde(gamma_data, gamma_optimal_bandwidth)
 bart_density_estimate <- kde(bart_data, bart_optimal_bandwidth)
 
-png("./png/gamma_loocv.png")
-plot(gamma_test_bandwidths, gamma_loocv_scores, pch = "•", cex = 1, 
-     col = "orange", xlab = TeX("$h$"), ylab = TeX("LOOCV Score"),
-     main = TeX("LOOCV Score as a Function of $h$"))
-abline(v = gamma_optimal_bandwidth, lwd = 3, col = "red")
-dev.off()
-
-png("./png/gamma_kde.png")
-plot(gamma_data, gamma_density_estimate, type = "l", col = "darkgreen", lwd = 3,
-     xlab = "x", ylab = TeX("$\\widehat{f}(x)"),
-     main = paste("Density Estimate: h =", signif(gamma_optimal_bandwidth)),
-     ylim = c(0, 0.3))
-lines(gamma_x_axis, dgamma(gamma_x_axis, 3, 1), col = "blue", lty = 3, lwd = 2)
-dev.off()
-
-png("./png/bart_loocv.png")
-plot(bart_test_bandwidths, bart_loocv_scores, pch = "•", cex = 1, 
-     col = "orange", xlab = TeX("$h$"), ylab = TeX("LOOCV Score"),
-     main = TeX("LOOCV Score as a Function of $h$"))
-abline(v = bart_optimal_bandwidth, lwd = 3, col = "red")
-dev.off()
-
-png("./png/bart_kde.png")
-plot(bart_data, bart_density_estimate, type = "l", col = "darkgreen", lwd = 3,
-     xlab = "x", ylab = TeX("$\\widehat{f}(x)"),
-     main = paste("Density Estimate: h =", signif(bart_optimal_bandwidth)),
-     ylim = c(0, 0.7))
-lines(bart_x_axis, dbart(bart_x_axis), col = "blue", lty = 3, lwd = 2)
-dev.off()
-
-
-# === MAKE GIF ===
 frame_num <- 0
+# make a gif for the gamma sample kde
 gamma_gif <- function(delay) {
     for (h in gamma_test_bandwidths) {
         if (h == gamma_optimal_bandwidth) {
@@ -173,7 +141,7 @@ gamma_gif <- function(delay) {
                  " ./png/gamma/*.png -loop 0 ./gamma.gif", sep = ""))
 }
 
-
+# make a gif for the bart sample kde
 bart_gif <- function(delay) {
     for (h in bart_test_bandwidths) {
         if (h == bart_optimal_bandwidth) {
@@ -204,20 +172,20 @@ bart_gif <- function(delay) {
             } else {
                 png(paste("./png/bart/kde_0", frame_num, ".png", sep = ""))
             }
-            plot(bart_data, kde(bart_data, h), type = "l", col = "red", 
+            plot(bart_data, kde(bart_data, h), type = "l", col = "red",
                  lwd = 3, xlab = "x", ylab = TeX("$\\widehat{f}(x)"),
                  main = paste("Density Estimate: h =", signif(h)),
                  ylim = c(0, 0.7))
-            lines(bart_x_axis, dbart(bart_x_axis), col = "blue", lty = 3, 
+            lines(bart_x_axis, dbart(bart_x_axis), col = "blue", lty = 3,
                   lwd = 2)
             dev.off()
             frame_num <- frame_num + 1
         }
     }
-    system(paste("convert -delay ", delay, 
+    system(paste("convert -delay ", delay,
                  " ./png/bart/*.png -loop 0 ./bart.gif", sep = ""))
 }
 
-
+# call gif-making functions
 gamma_gif(4)
 bart_gif(4)
